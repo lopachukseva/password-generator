6040aaa8f7f145643343ec5c7e611d70780fc8cb
Seva Lopachuk <lopachuk.seva@gmail.com>
2023-03-25 13:45:43 +0100

    add event listeners to buttons

---
+++ js/script.js
@@ -0,0 +1,65 @@
+numSwitch = document.getElementById('switch1');
+symSwitch = document.getElementById('switch2');
+uppSwitch = document.getElementById('switch3');
+lowSwitch = document.getElementById('switch4');
+rangeSlider = document.getElementById('len-slider');
+
+passScreen = document.querySelector('.password-screen p');
+genButton = document.querySelector('.generate-button');
+
+
+passwordLengthScreen = document.querySelector('.symbols-count-label .count');
+
+
+function generatePassword() {
+    return 'something';
+};
+
+rangeSlider.addEventListener("input", () => {
+    passwordLengthScreen.textContent = rangeSlider.value;
+});
+
+numSwitch.addEventListener("input", () => {
+    if (numSwitch.checked){
+        console.log('num is on!');
+    }
+    else {
+        console.log('num is off!');
+    }
+        
+});
+
+symSwitch.addEventListener("input", () => {
+    if (symSwitch.checked){
+        console.log('sym is on!');
+    }
+    else {
+        console.log('sym is off!');
+    }
+        
+});
+
+uppSwitch.addEventListener("input", () => {
+    if (uppSwitch.checked){
+        console.log('upp is on!');
+    }
+    else {
+        console.log('upp is off!');
+    }
+        
+});
+
+lowSwitch.addEventListener("input", () => {
+    if (lowSwitch.checked){
+        console.log('low is on!');
+    }
+    else {
+        console.log('low is off!');
+    }
+        
+});
+
+genButton.addEventListener("click", () => {
+    password = generatePassword();
+    passScreen.textContent = password;
+});
